9a261863a5b4b3fa7d2f9b763a8b4e7c8ccc1f9c
abir cherif <abir.cherif@internship.continuousnet.com>
2024-10-25 12:23:44 +0100

    contact section

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const aboutButtons = document.querySelectorAll('.about-btn button');
+//   const contents = document.querySelectorAll('.content');
+
+//   aboutButtons.forEach((btn, idx) => {
+//       btn.addEventListener('click', () => {
+//           aboutButtons.forEach(button => button.classList.remove('active'));
+//           btn.classList.add('active');
+//           contents.forEach(content => content.classList.remove('active'));
+//           contents[idx].classList.add('active');
+//       });
+//   });
+// });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   var options = {
+//       strings: ["Frontend Developer 🚀", "Flutter Developer", "Web Developer"],
+//       // strings: ["Frontend Developer 🚀"],
+//       typeSpeed: 60,
+//       backSpeed: 30,
+//       backDelay: 1500,
+//       startDelay: 500,
+//       loop: true
+//   };
+
+//   var typed = new Typed("#animated-text", options);
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const aboutButtons = document.querySelectorAll('.about-btn button');
   const contents = document.querySelectorAll('.content');
@@ -10,12 +38,9 @@ document.addEventListener("DOMContentLoaded", function () {
           contents[idx].classList.add('active');
       });
   });
-});
 
-document.addEventListener('DOMContentLoaded', () => {
   var options = {
       strings: ["Frontend Developer 🚀", "Flutter Developer", "Web Developer"],
-      // strings: ["Frontend Developer 🚀"],
       typeSpeed: 60,
       backSpeed: 30,
       backDelay: 1500,
